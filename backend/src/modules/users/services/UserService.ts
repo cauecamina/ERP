3194e194b4554f182778d33be7da1f9aedd169d0
Cauê Camina <cauecamina@hotmail.com>
2026-03-27 01:52:57 -0300

    trocando biblioteca

--- backend/src/modules/users/services/UserService.ts
+++ backend/src/modules/users/services/UserService.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { userRepository } from "../repositories/UserRepository";
 import { CustomError } from "../../../middlewares/errorMiddleware";
